45ddc9a192fd8b3bdfa09ca21ee4034d6b6dd426
Carlos Toledo <carlombo@hotmail.com>
2024-03-20 22:57:06 -0300

    Actualización

--- apuntes.py
+++ apuntes.py
@@ -3,7 +3,7 @@
 # Manuel
 # Pérez
 # Toro'''
-# print(nombre) 
+# print(nombre)
 
 
 
@@ -26,8 +26,8 @@
 
 
 # # Transformar integer o float
-# inte = int(input('Ingrese un número (integer): \n ')) 
-# floa = float(input('Ingrese un número (float): \n ')) 
+# inte = int(input('Ingrese un número (integer): \n '))
+# floa = float(input('Ingrese un número (float): \n '))
 # print ('Nunmero integer: ' + str(inte))
 # print ('Nunmero float: ' + str(floa))
 
@@ -128,8 +128,8 @@
 # print(len('Grupo musical de metal  5')) #Cuenta llos carácteres inculuido espacios
 # print('santana'.upper()) #Todas en máyusculas
 # print('HolA cOmo EstÁS '.lower()) #Todas en minúsculas
-# print('carlos santana'.title()) #Mayúscula las primeras letras 
-# print('hola a todos'.capitalize()) #la primera letra 
+# print('carlos santana'.title()) #Mayúscula las primeras letras
+# print('hola a todos'.capitalize()) #la primera letra
 # print(' Hola a todo, cómo están '.strip()) #elimina los espacios al final o al principio
 # print('Hola mundo'.replace('o', 'x')) #reemplaza las ocurrencias
 # print(' -> '.join(['papel', 'piedra', 'tijera'])) #Une elementos con el string
@@ -152,7 +152,7 @@
 # #Separador usando un #
 # aaa = "carlos#santana#cantante#de#musica"
 # separado = aaa.split('#')  # Usando la coma como separador
-# print(separado) 
+# print(separado)
 
 
 
@@ -168,14 +168,98 @@
 #         print('Este password es muy sencillo. Elija otro')
 #     elif password == '87654321':
 #         print('Este password es muy sencillo. Elija otro')
-#     elif "'" in password or "*" in password or "-" in password:
+#     elif ("'" in password) or ("*" in password) or ("-" in password):
 #         print("No puede usar especial dentro de su password")
 #     else:
 #         print('Contraseña correcta')
-
 
 # #Le pido la nueva contraseña la usuario
 # password = input('Ingrese una contraseña: ')
 
 # #Valido la contraseña con la función "validarPassword"
 # validarPassword(password)
+
+
+
+
+
+# import getpass
+
+# def validarPassword(password): #Acoplar funciones para hacer más fácil revisar, arreglar
+#     #Validamos que sea de largo al menos sea 6
+#     if len(password) < 6:
+#         print('Su contraseña debe contener al menos 6 caracteres')
+#         return False
+#     elif password == '12345678':
+#         print('Este password es muy sencillo. Elija otro')
+#         return False
+#     elif password == '87654321':
+#         print('Este password es muy sencillo. Elija otro')
+#         return False
+#     elif ("'" in password) or ("*" in password) or ("-" in password):
+#         print("No puede usar especial dentro de su password")
+#         return False
+#     else:
+#         print('Contraseña correcta')
+#         return True
+
+# def main():
+# # esCorrecta = False
+# # while esCorrecta != True:
+#     while True:
+#         # 1. Le pido la nueva contraseña la usuario
+#         password = getpass.getpass('Ingrese una contraseña: ')
+#         print(f'Password ingresado: {password}')
+#         # Valido la contraseña con la función "validarPassword"
+#         esCorrecta = validarPassword(password) 
+#         # Si la contraseña es incorretca, repite el ciclo. Si está bien, salgo del ciclo con break
+#         if esCorrecta:
+#             break # Salir del ciclo
+#     print('Bienvenido!') # Fuera del ciclo
+
+# main()
+
+
+
+
+
+
+
+# numero = None
+# while numero is None:
+#     try:
+#         numero = int(input("Ingresa un número: "))
+#     except ValueError:
+#         print("Eso no es un número válido.")
+
+
+
+
+# import sys
+# def sigma():
+#     numero = int(sys.argv[1])
+#     i = 1 #inicar iterador
+#     suma = 0
+#     while i <= numero: #condición
+#         suma += i
+#         i += 1 #Incremento
+#     print(suma) 
+# sigma()
+
+
+
+
+
+# def sigmafor():
+#     suma = 0
+#     for i in range(1, 101, 1): #el for se ocupa con range: inicio, final+1, salto
+#         suma += i
+#     print(suma)
+# sigmafor()
+
+
+
+
+
+# for letra in 'hola como estas':
+#     print(letra)
