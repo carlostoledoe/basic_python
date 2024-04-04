933ccac4bc3087f232cfff898bcb4ce7e5a54088
Carlos Toledo <carlombo@hotmail.com>
2024-04-03 22:57:04 -0300

    Actualización

---
+++ 27 API.py
@@ -0,0 +1,25 @@
+# JSON : JavaScript Object Notation
+# curl hace solicitudes a una api dese el terminal
+# Otra forma es la web
+
+
+
+
+
+
+import requests
+import json
+
+# url = 'https://anapioficeandfire.com/api/characters/583'
+# response = requests.get(url)
+# # print(response.text) # Imprime solo texto, ya que llega como texto
+# jon_snow = json.loads(response.text) # Lo transforma en un diccionario
+# print('Estos son los apodos de Jon Snow:')
+# for apodo in jon_snow['aliases']:
+#     print(apodo)
+
+
+
+
+
+
