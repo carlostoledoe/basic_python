21ea35def72b1385242c5bbba7746aa867541388
Carlos Toledo <carlombo@hotmail.com>
2024-04-04 23:53:22 -0300

    Actualización

--- 27 API.py
+++ 27 API.py
@@ -1,14 +1,30 @@
 # JSON : JavaScript Object Notation
 # curl hace solicitudes a una api dese el terminal
-# Otra forma es la web
+# Otra forma es la web Yarc
+# Es por Python
 
 
+# 200 OK: La solicitud ha tenido éxito.
+# 201 Created: Se ha creado un nuevo recurso como resultado de la solicitud (por ejemplo, después de una petición PUT).
+# 204 No Content: La solicitud se ha completado con éxito, pero la respuesta no tiene contenido (aunque los encabezados pueden ser útiles).
+# 400 Bad Request: La solicitud contiene una sintaxis no válida o el servidor no puede cumplirla.
+# 401 Unauthorized: El cliente debe proporcionar credenciales de autenticación.
+# 404 Not Found: El recurso solicitado no existe en el servidor.
+# 500 Internal Server Error: Hubo un error interno en el servidor
 
+
+# json.loads  para trasnformar de JSON (texto) a un diccionario de Python
+# json.dumps  para transformar desde un diccionario Python a JSON (texto)
 
 
 
 import requests
 import json
+import pdn
+
+
+
+
 
 # url = 'https://anapioficeandfire.com/api/characters/583'
 # response = requests.get(url)
@@ -23,3 +39,39 @@ import json
 
 
 
+
+# #Requerimiento simple
+# def request_get(url): 
+#     return json.loads(requests.get(url).text) 
+
+# url = 'https://reqres.in/api/users'
+
+# users_data = request_get(url)
+# print(json.dumps(users_data, indent=4))
+
+
+
+
+
+
+
+
+
+
+
+url = 'https://jsonplaceholder.typicode.com/posts'
+data = {
+    'userID': 1,
+    'title': 'hola',
+    'body': 'Este es mi primer posteo desde Python'
+}
+
+data = json.dumps(data)
+headers = {
+    'Content-Type': 'application/json'
+}
+
+response = requests.post(url, headers, data) #Envía la información
+print(response)
+print(response.text)
+#pdb.set_trace() #response.status_code // response.text
---
+++ Desafios/08/ave_api.py
@@ -0,0 +1,65 @@
+import requests
+import json
+
+#Consulta la api y la convierte en json
+def request_get(url): 
+    return json.loads(requests.get(url).text) 
+
+
+# Crea un html a partir del template
+def crear_html(template):
+    with open('index.html', 'w') as f:
+        return f.write(template)
+
+
+
+# Url a consultar
+url =  'https://aves.ninjas.cl/api/birds'
+photos = request_get(url)
+
+
+
+# Armado de la Card
+card = ''
+for photo in photos:
+    card += f'''<div class="col-3">
+                    <div class="card" style="width: 12rem;">
+                        <img src="{photo['images']['thumb']}" class="card-img-top" alt="{photo['name']['english']}">
+                        <div class="card-body">
+                            <h5 class="card-title">{photo['name']['spanish']}</h5>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">En ingles: {photo['name']['english']}</li>
+                            <li class="list-group-item">En latín: {photo['name']['latin']}</li>
+                        </ul>
+                        <div class="card-body">
+                            <a href="{photo['images']['full']}" class="card-link">Ver en grande</a>
+                        </div>
+                    </div>
+                </div>
+'''
+
+
+# Template HTML
+html_template = f'''<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Aves Chile</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim aspernatur perspiciatis cumque aliquam alias perferendis voluptates. Sunt fuga inventore, nesciunt dolor sapiente, ab dignissimos voluptas unde eligendi asperiores, ad animi!</p>
+            <div class="row">
+                {card}
+            </div>
+
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>'''
+
+crear_html(html_template)
